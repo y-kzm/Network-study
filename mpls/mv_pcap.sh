2f3498939b2f5135b954883cc59b2334c40aca2d
y-kzm <softietennis0212kazuma@gmail.com>
2021-11-02 13:51:44 +0900

    re pcap.

--- mpls/mv_pcap.sh
+++ mpls/mv_pcap.sh
@@ -2,7 +2,10 @@
 echo "Start pcap."
 docker cp R0:/tmp/R0_net0.pcap R0_net0.pcap  
 docker cp R2:/tmp/R2_net1.pcap R2_net1.pcap   
+docker cp R2:/tmp/R2_net0.pcap R2_net0.pcap   
+docker cp R2:/tmp/R2_net2.pcap R2_net2.pcap   
 docker cp R4:/tmp/R4_net0.pcap R4_net0.pcap   
+docker cp R4:/tmp/R4_net1.pcap R4_net1.pcap   
 docker cp R5:/tmp/R5_net0.pcap R5_net0.pcap   
 docker cp R3:/tmp/R3_net2.pcap R3_net2.pcap   
 docker cp R3:/tmp/R3_net0.pcap R3_net0.pcap   
