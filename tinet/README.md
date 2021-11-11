a2d99e918c39539b830b5d1751dafd283664c543
y-kzm <softietennis0212kazuma@gmail.com>
2021-11-11 12:31:51 +0900

    Add pcap.

---
+++ tinet/README.md
@@ -0,0 +1,1 @@
+# Network-Study using tinet.
--- tinet/ldp/mv_pcap.sh
+++ tinet/ldp/mv_pcap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start pcap."
-docker cp R1:/tmp/R0_net0.pcap R1_net0.pcap  
-docker cp R2:/tmp/R0_net0.pcap R2_net0.pcap  
+docker cp R1:/tmp/R1_net0.pcap pcap/R1_net0.pcap  
+docker cp R2:/tmp/R2_net0.pcap pcap/R2_net0.pcap  
