f706a1d7f4cf6e4595970f15029c638f7987dbd7
y-kzm <softietennis0212kazuma@gmail.com>
2021-11-15 13:40:59 +0900

    Add topo.

---
+++ tinet/ospfv2_single/mv_pcap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Start pcap."
+docker cp R1:/tmp/R1_net0.pcap pcap/R1_net0.pcap  
+docker cp R1:/tmp/R1_net1.pcap pcap/R1_net1.pcap  
+docker cp R2:/tmp/R2_net0.pcap pcap/R2_net0.pcap  
+docker cp R2:/tmp/R2_net1.pcap pcap/R2_net1.pcap 
+docker cp R3:/tmp/R3_net0.pcap pcap/R3_net0.pcap  
+docker cp R2:/tmp/R2_net1.pcap pcap/R3_net1.pcap 
