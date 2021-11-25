501a438dff12c5b05dec32507dded7cdf4d62735
y-kzm <softietennis0212kazuma@gmail.com>
2021-11-25 22:53:08 +0900

    Update files.

--- tinet/srmpls/mv_pcap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Start pcap."
-docker cp R2:/tmp/R2_net0.pcap pcap/R2_net0.pcap  
-docker cp R2:/tmp/R2_net1.pcap pcap/R2_net1.pcap   
-docker cp R2:/tmp/R2_net2.pcap pcap/R2_net2.pcap   
-docker cp R4:/tmp/R4_net0.pcap pcap/R4_net0.pcap   
-docker cp R4:/tmp/R4_net1.pcap pcap/R4_net1.pcap   
-docker cp R4:/tmp/R4_net2.pcap pcap/R4_net2.pcap   
